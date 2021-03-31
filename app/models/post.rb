1c574d0557148e1e88f216d4578b62cfcb03b7c9
Alamgir <ourandoc@gmail.com>
2021-03-31 14:17:34 +0500

    Add migration associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
-    belongs_to: user
-    has_many: comments
+    belongs_to :user
+    has_many :comments
+    validates :title, presence: true, length: { maximum: 20}
+    validates :body, presence: true, length: { maximum: 50}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    has_many: comments
-    has_many: posts
+    has_many :comments
+    has_many :posts
+    validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+    validates :email, presence: true
 end
