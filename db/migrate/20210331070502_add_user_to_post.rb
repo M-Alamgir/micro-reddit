4ce28ea1f83e474144a1c5e27e962620559d17cb
Alamgir <ourandoc@gmail.com>
2021-03-31 15:41:43 +0500

    fix linters

--- db/migrate/20210331070502_add_user_to_post.rb
+++ db/migrate/20210331070502_add_user_to_post.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddUserToPost < ActiveRecord::Migration[6.1]
   def change
     add_reference :posts, :user, null: false, foreign_key: true
--- db/migrate/20210331072409_add_user_and_post_to_comment.rb
+++ db/migrate/20210331072409_add_user_and_post_to_comment.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddUserAndPostToComment < ActiveRecord::Migration[6.1]
   def change
     add_reference :comments, :user, null: false, foreign_key: true
