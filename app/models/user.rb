35f52e8b3d02f14ebfe1fb5b1a3680e683c7e4bc
Alamgir <ourandoc@gmail.com>
2021-03-31 15:24:37 +0500

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_many :comments
-    has_many :posts
-    validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
-    validates :email, presence: true
+  has_many :comments
+  has_many :posts
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :email, presence: true
 end
